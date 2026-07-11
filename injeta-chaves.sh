9108def9d715662998b82268ee68a0e095eed763
Giselle Oliveira <adm-giselleoliveira@outlook.com>
2026-07-11 01:52:17 -0300

    Corrige script para criar env-config.js

--- injeta-chaves.sh
+++ injeta-chaves.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Gera o arquivo env-config.js com as chaves que vem do GitHub Secrets
 echo "window._env_ = { APIKEY_TMDB: '${APIKEY_TMDB}', APIKEY_OMDB: '${APIKEY_OMDB}' };" > env-config.js
