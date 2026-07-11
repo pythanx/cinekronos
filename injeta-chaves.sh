ad77510c5485510cbd5404c096a50e432cf3d8ee
Giselle Oliveira <adm-giselleoliveira@outlook.com>
2026-07-11 11:48:35 -0300

    Adiciona debug no script

--- injeta-chaves.sh
+++ injeta-chaves.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "=== CRIANDO env-config.js ==="
 echo "window._env_ = { APIKEY_TMDB: '${APIKEY_TMDB}', APIKEY_OMDB: '${APIKEY_OMDB}' };" > env-config.js
+echo "=== ARQUIVO CRIADO ==="
+cat env-config.js
